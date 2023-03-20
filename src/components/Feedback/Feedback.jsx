fc5385b7f64ae08d2e2d3982d5d0503ec8ae3d5c
Gaultheria <valeriaburina@gmail.com>
2023-03-20 17:08:02 +0100

    step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -5,8 +5,8 @@ export class Feedback extends Component {
               good: 0,
               neutral: 0,
               bad: 0
-        } 
-
+        }    
+    
     handleGoodButton = () => {
         this.setState({
             good: this.state.good +1
@@ -23,7 +23,18 @@ export class Feedback extends Component {
         })
     }
     
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state; 
+        return good + neutral + bad;
+    } 
+    
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state; 
+        return Math.round((good / this.countTotalFeedback()) * 100);
+    }
+
     render() {
+
         return (
             <div>
                 <div className='bottonsBlock'>
@@ -36,6 +47,7 @@ export class Feedback extends Component {
                     <p className='goodCounter'> Good: { this.state.good } </p>
                     <p className='neutralCounter'> Neutral: { this.state.neutral } </p>
                     <p className='badCounter'> Bad: { this.state.bad } </p>
+                    <p className='positivePercentage '> Positive feedback: { this.countPositiveFeedbackPercentage() } </p>
                 </div>
 
             </div>
